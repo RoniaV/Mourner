5fe32cb91d15863ab0fcd94850e78149d2e4e86e
RoniaV <r.r.vento@gmail.com>
2024-01-10 10:42:36 +0100

    Fix Bell Local Axis

--- Mourner Project/Assets/MiscScrips/FollowTarget.cs
+++ Mourner Project/Assets/MiscScrips/FollowTarget.cs
@@ -7,10 +7,14 @@ public class FollowTarget : MonoBehaviour
     [SerializeField] Transform target;
     [SerializeField] Vector3 offset = Vector3.zero;
     [SerializeField] bool localOffset = true;
+    [SerializeField] bool setRotation = false;
 
     void LateUpdate()
     {
         Vector3 followPos = localOffset ? target.TransformPoint(offset) : target.position + offset;
         transform.position = followPos;
+
+        if (setRotation)
+            transform.rotation = target.root.rotation;
     }
 }
